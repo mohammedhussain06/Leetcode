cc78a5736a8c6e209cf5d0c7665c245628a694c6
Hussain <hussain2006.shaikh@gmail.com>
2026-07-03 19:01:58 +0530

    Time: 2 ms (100.00%) | Memory: 46.2 MB (77.26%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,14 +1,44 @@
 class Solution {
     public String intToRoman(int num) {
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        StringBuilder roman = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                roman.append(symbols[i]);
-            }
+        char[] buf = new char[15];
+        int pos = 0;
+        int thousands = num / 1000;
+        while (thousands-- > 0) {
+            buf[pos++] = 'M';
         }
-        return roman.toString();
+        int hundreds = (num % 1000) / 100;
+        if (hundreds == 9) {
+            buf[pos++] = 'C'; buf[pos++] = 'M';
+        } else if (hundreds >= 5) {
+            buf[pos++] = 'D';
+            while (hundreds-- > 5) buf[pos++] = 'C';
+        } else if (hundreds == 4) {
+            buf[pos++] = 'C'; buf[pos++] = 'D';
+        } else {
+            while (hundreds-- > 0) buf[pos++] = 'C';
+        }
+        int tens = (num % 100) / 10;
+        if (tens == 9) {
+            buf[pos++] = 'X'; buf[pos++] = 'C';
+        } else if (tens >= 5) {
+            buf[pos++] = 'L';
+            while (tens-- > 5) buf[pos++] = 'X';
+        } else if (tens == 4) {
+            buf[pos++] = 'X'; buf[pos++] = 'L';
+        } else {
+            while (tens-- > 0) buf[pos++] = 'X';
+        }
+        int ones = num % 10;
+        if (ones == 9) {
+            buf[pos++] = 'I'; buf[pos++] = 'X';
+        } else if (ones >= 5) {
+            buf[pos++] = 'V';
+            while (ones-- > 5) buf[pos++] = 'I';
+        } else if (ones == 4) {
+            buf[pos++] = 'I'; buf[pos++] = 'V';
+        } else {
+            while (ones-- > 0) buf[pos++] = 'I';
+        }
+        return new String(buf, 0, pos);
     }
 }
