de19e001198fff8ea601dd60332071db07587f05
Hussain <hussain2006.shaikh@gmail.com>
2026-07-10 20:13:27 +0530

    Time: 2 ms (83.40%) | Memory: 46.7 MB (28.83%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(board, 0, result);
+        return result;
+    }
+
+    private void backtrack(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(constructBoard(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q'; 
+                backtrack(board, row + 1, result);
+                board[row][col] = '.';      
+            }
+        }
+    }
+    private boolean isSafe(char[][] board, int row, int col) {
+        // Vertically up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Diagonal-left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Diagonal-right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    private List<String> constructBoard(char[][] board) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+}
